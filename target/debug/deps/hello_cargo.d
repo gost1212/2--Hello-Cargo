37a299e5805a177f7769876a84769da1981858ab
gost1212 <saadalgamdi888@gmail.com>
2024-01-08 19:00:51 +0300

    first commit

---
+++ target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Saad_\Documents\Rust\Excersises\2--Hello-Cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\Saad_\Documents\Rust\Excersises\2--Hello-Cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Saad_\Documents\Rust\Excersises\2--Hello-Cargo\target\debug\hello_cargo.exe: C:\Users\Saad_\Documents\Rust\Excersises\2--Hello-Cargo\src\main.rs
